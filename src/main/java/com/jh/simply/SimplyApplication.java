fb49f7382d159bf98599985f4ebeb549bf3ed9b2
jh <ss1m6a@gmail.com>
2023-04-19 00:41:17 +0900

    first commit

---
+++ src/main/java/com/jh/simply/SimplyApplication.java
@@ -0,0 +1,13 @@
+package com.jh.simply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jh/simply/SimplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.simply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
